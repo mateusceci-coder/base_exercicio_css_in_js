05282a9d8c022ec3191f0a3fbb13437723341f73
Mateus Ceci <mateusceci@gmail.com>
2023-08-09 16:57:17 -0300

    fix buttons

--- src/components/Vaga/styles.ts
+++ src/components/Vaga/styles.ts
@@ -1,24 +1,6 @@
 import styled from 'styled-components'
 import { GlobalColors } from '../../Global'
 
-export const VagaLi = styled.li`
-  border: 1px solid ${GlobalColors.corPrincipal};
-  background-color: ${GlobalColors.corSecundaria};
-  color: ${GlobalColors.corPrincipal};
-  padding: 16px;
-  transition: all ease 0.3s;
-  border-radius: 8px;
-
-  :hover {
-    background-color: ${GlobalColors.corPrincipal};
-    color: ${GlobalColors.corSecundaria};
-  }
-`
-export const VagaTitulo = styled.h3`
-  font-weight: bold;
-  margin-bottom: 16px;
-`
-
 export const VagaLink = styled.a`
   border-color: ${GlobalColors.corSecundaria};
   background-color: ${GlobalColors.corPrincipal};
@@ -32,13 +14,32 @@ export const VagaLink = styled.a`
   border-radius: 8px;
   text-align: center;
 
-  :hover {
-    border-color: ${GlobalColors.corPrincipal};
-    background-color: ${GlobalColors.corSecundaria};
-    color: ${GlobalColors.corPrincipal};
-  }
-
   @media (max-width: 768px) {
     display: block;
   }
+`
+
+export const VagaLi = styled.li`
+  border: 1px solid ${GlobalColors.corPrincipal};
+  background-color: ${GlobalColors.corSecundaria};
+  color: ${GlobalColors.corPrincipal};
+  padding: 16px;
+  transition: all ease 0.3s;
+  border-radius: 8px;
+
+  :hover {
+    background-color: ${GlobalColors.corPrincipal};
+    color: ${GlobalColors.corSecundaria};
+
+    ${VagaLink} {
+      border-color: ${GlobalColors.corPrincipal};
+      background-color: ${GlobalColors.corSecundaria};
+      color: ${GlobalColors.corPrincipal};
+      cursor: pointer;
+    }
+  }
+`
+export const VagaTitulo = styled.h3`
+  font-weight: bold;
+  margin-bottom: 16px;
 `
